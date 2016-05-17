34860b7e653e46d06b3fe8710d21df2e19ae65b6
marlonavila29 <marlonavila29@gmail.com>
2016-05-17 19:26:58 -0400

    2 check in
    2

--- app/src/main/java/uniderp/controller/ActCadastro.java
+++ app/src/main/java/uniderp/controller/ActCadastro.java
@@ -1,12 +1,8 @@
 package uniderp.controller;
 
-import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,30 +16,19 @@ public class ActCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_cadastro);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
-    public void save(View v){
+    public void salvarCompromisso(View v){
         final AcessoBanco db = new AcessoBanco(this);
-        Button buttonSalvar = (Button) findViewById(R.id.buttonSalvar);
+       // Button buttonSalvar = (Button) findViewById(R.id.buttonSalvar);
         EditText editDataEvento = (EditText) findViewById(R.id.editDataEvento);
-        EditText editHoraEvento = (EditText) findViewById(R.id.editHoraEvento);
+        /*EditText editHoraEvento = (EditText) findViewById(R.id.editHoraEvento);
         EditText editHoraFim = (EditText) findViewById(R.id.editHoraFim);
         EditText editLocalEvento = (EditText) findViewById(R.id.editLocalEvento);
         EditText editDescricao = (EditText) findViewById(R.id.editDescricao);
         EditText editParticipantes = (EditText) findViewById(R.id.editParticipantes);
-
+*/
         db.open();
-        db.inserePessoa(editDataEvento.toString());
+        db.inserePessoa(editDataEvento.getText().toString());
         db.close();
 
         db.open();
@@ -52,7 +37,7 @@ public class ActCadastro extends AppCompatActivity {
         if (c.moveToFirst())
         {
             do {
-               mostraRegistro(c);
+                mostraRegistro(c);
 
             } while (c.moveToNext());
         }
