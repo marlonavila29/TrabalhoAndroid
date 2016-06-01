b34cde75d25dc9c82e08932c05ef17d7b8e049e7
kahalmeida <karoll.6@hotmail.com>
2016-05-31 22:51:07 -0300

    Botao Expurgar
    Botao Expurgar

--- app/src/main/java/uniderp/controller/MainActivity.java
+++ app/src/main/java/uniderp/controller/MainActivity.java
@@ -1,8 +1,11 @@
 package uniderp.controller;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -47,5 +50,39 @@ public class MainActivity extends AppCompatActivity {
     public void cadastrarCompromisso(View v){
         Intent actCadastro = new Intent(this, ActCadastro.class);
         startActivity(actCadastro);
+    }
+    public void expurgarCompromissos(View v){
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.input_nova_repeticao, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("sim", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // PEgar a data selecionada no calendario
+
+                        //Trazer do banco todos os compromisso (getCompromisso)
+
+                        //Passar Elementos do tipo CUrsor para Array List
+
+                        //Ver na arralist quais datas são menor ou igual a data selecionada
+
+                        //Pesquisar no banco o ID do compromisso passando a Data como parametro
+
+                        //removerCompromisso()
+
+
+                    }
+                })
+                .setNegativeButton("cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
