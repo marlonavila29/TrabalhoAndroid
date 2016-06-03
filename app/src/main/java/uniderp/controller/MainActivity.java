066c949753ad13911abe83e47aedc7e387cc367b
marlonavila29 <marlonavila29@gmail.com>
2016-06-02 21:24:43 -0400

    Versão 8.1
    * Preparaçao para a carol

--- app/src/main/java/uniderp/controller/MainActivity.java
+++ app/src/main/java/uniderp/controller/MainActivity.java
@@ -3,6 +3,7 @@ package uniderp.controller;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -13,17 +14,19 @@ import android.widget.CalendarView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import uniderp.appeventos.R;
+import uniderp.model.Compromisso;
 
 
 public class MainActivity extends AppCompatActivity {
 
     CalendarView calendar;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +34,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         calendar = (CalendarView) findViewById(R.id.calendar);
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
-        {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
-
-
-            }
-        });
-
-
     }
-    public void visualizarCompromissos(View v){
+
+    public void visualizarCompromissos(View v) {
         Intent actCompromissos = new Intent(this, ActCompromissos.class);
         startActivity(actCompromissos);
     }
-    public void cadastrarCompromisso(View v){
+
+    public void cadastrarCompromisso(View v) {
         Intent actCadastro = new Intent(this, ActCadastro.class);
         startActivity(actCadastro);
     }
-    public void expurgarCompromissos(View v){
+
+    public void expurgarCompromissos(View v) {
         LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
         View promptView = layoutInflater.inflate(R.layout.input_nova_repeticao, null);
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
@@ -60,17 +55,67 @@ public class MainActivity extends AppCompatActivity {
         alertDialogBuilder.setCancelable(false)
                 .setPositiveButton("sim", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // PEgar a data selecionada no calendario
+                        //Declaração de variáveis
+                        final Date dataSelecionada = null;
+                        Date dataEvento;
+                        List<Integer> codigosCompromissoParaExcluir = new ArrayList<Integer>();
+                        List<Compromisso> listaCompromisso = new ArrayList<Compromisso>();
+                        Compromisso compromisso;
+                        //Aqui é instanciado a Casse Acesso ao baco. Usa-se o "db" para chamar os métodos dessa classe
+                        AcessoBanco db = new AcessoBanco(getApplicationContext());
 
-                        //Trazer do banco todos os compromisso (getCompromisso)
+                        //Pegando a data selecionada do calendario e passando para a variavel dataSelecionada
+                        calendar = (CalendarView) findViewById(R.id.calendar);
+                        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                            @Override
+                            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                                dataSelecionada.setDate(dayOfMonth);
+                                dataSelecionada.setMonth(month);
+                                dataSelecionada.setYear(year);
+                            }
+                        });
 
-                        //Passar Elementos do tipo CUrsor para Array List
+                        Cursor c = null;
+                        //FAZER: Trazer do banco todos os compromisso  (getCompromisso) e "colocar" na variavel "c"
 
-                        //Ver na arralist quais datas são menor ou igual a data selecionada
+                        /*Pegando os compromissos cadastrados da variavel "c" que veio do banco e colocando no objeto e
+                        inserindo cada compromisso numa lista de compromissos */
+                        if (c != null && c.getCount() > 0) {
+                            do {
+                                compromisso = new Compromisso();
+                                compromisso.setIdCompromisso(c.getInt(0));
+                                compromisso.setDataEvento(c.getString(1));
+                                compromisso.setHoraInicio(c.getInt(2));
+                                compromisso.setHoraFim(c.getInt(3));
+                                compromisso.setLocalRealizacao(c.getString(4));
+                                compromisso.setDescricao(c.getString(5));
+                                compromisso.setParticipantes(c.getString(6));
+                                compromisso.setIdTipoEvento(c.getInt(7));
+                                listaCompromisso.add(compromisso);
+                            } while (c.moveToNext());
+                        }
+                        //Trazendo de dentor da lista, um por um, cada compromisso cadastrado para fazer a verificacao das datas
+                        for (int i = 0; i < listaCompromisso.size(); i++) {
+                            compromisso = new Compromisso();
+                            compromisso = listaCompromisso.get(i);
+                            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                            sdf.setLenient(false);
+                            try {
+                                dataEvento = sdf.parse(compromisso.getDataEvento());
 
-                        //Pesquisar no banco o ID do compromisso passando a Data como parametro
+                            } catch (ParseException e) {
+                            }
+                            //FAZER: Comparar se "dataEvento" é menor ou igual à "dataSelecionada"
+                            //FAZER: Se for igual, execute isso: codigosCompromissoParaExcluir.add(compromisso.getIdCompromisso())
+                        }
 
-                        //removerCompromisso()
+                        //removendo os compromissos menores ou iguais a data selecionada no calendario
+                        for (int i = 0; i < codigosCompromissoParaExcluir.size(); i++) {
+                            //FAZER: Abra a conexao
+                            //FAZER: chame o método da Classe AcessoBanco "removerCompromisso() e passe como parâmetro isso:  codigosCompromissoParaExcluir.get(i);
+                            //FAZER: fechar conexao
+
+                        }
 
 
                     }
